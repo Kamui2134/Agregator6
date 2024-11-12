2a4229389272bf928f503f07118aa07cdbd57aec
Шичков Александр <ctorkey@gmail.com>
2024-11-12 19:32:27 +0300

    all media are done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,22 @@
-"use strict"
+'use strict'
 
 const bgImg1 = document.querySelector('.bg-img-1')
 
-window.addEventListener("scroll", () => {
-    bgImg1.style.backgroundPositionY = `${window.scrollY * 0.3}px`
+window.addEventListener('scroll', () => {
+	bgImg1.style.backgroundPositionY = `${window.scrollY * 0.3}px`
+})
+
+const nav = document.querySelector('.header__nav')
+const navToggler = document.querySelector('.header__toggle-btn')
+const navLinks = document.querySelectorAll('.header__nav-link')
+
+navToggler.addEventListener('click', function () {
+	this.classList.toggle('active')
+	nav.classList.toggle('active')
+})
+navLinks.forEach(element => {
+	element.addEventListener('click', function () {
+		nav.classList.remove('active')
+		navToggler.classList.remove('active')
+	})
 })
